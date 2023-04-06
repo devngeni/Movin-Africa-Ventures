c1f3f4bd24235b5a18a78797f02da008683f2ba9
4SAMU <samuelnzomo4@gmail.com>
2023-04-06 16:59:57 +0300

    update on gotham bold fonts

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "096caaea99e79d5a"; }
+/******/ 		__webpack_require__.h = function() { return "6d1ac98ac1669440"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
