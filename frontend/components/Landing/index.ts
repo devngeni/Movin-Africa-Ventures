3f9efea4f594526675cf8683184a95e8f5f802c5
4SAMU <samuelnzomo4@gmail.com>
2023-04-06 03:20:12 +0300

    updated upto footer page

--- frontend/components/Landing/index.ts
+++ frontend/components/Landing/index.ts
@@ -4,3 +4,5 @@ export { default as WeAre } from "../Landing/WeAre/WeAre";
 export { default as ValueAdd } from "../Landing/ValueAdd/ValueAdd";
 export { default as WhyNow } from "../Landing/WhyNow/WhyNow";
 export { default as Investments } from "../Landing/Investments/Investments";
+export { default as Team } from "../Landing/Team/Team";
+export { default as Footer } from "../Landing/Footer/Footer";
