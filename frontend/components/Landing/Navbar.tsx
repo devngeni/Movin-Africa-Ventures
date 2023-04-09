a8d2f334ee1eb5a617444f3e99b0a696632cee35
4SAMU <samuelnzomo4@gmail.com>
2023-04-09 09:32:08 +0300

    responsiveness on navbar

--- frontend/components/Landing/Navbar.tsx
+++ frontend/components/Landing/Navbar.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Moving from "../../public/Moving.png";
 import Image from "next/image";
 import styles from "@/styles/Navbar.module.css";
+import { FiMenu } from "@react-icons/all-files/fi/FiMenu";
 
 export default function Navbar() {
-  const { nav, logo, mini_nav } = styles;
+  const { nav, logo, mini_nav, Menu_icon } = styles;
   return (
     <div className={nav}>
       <Image className={logo} src={Moving} alt="moving -icon" />
+      <div className={Menu_icon}>
+        <FiMenu />
+      </div>
       <div className={mini_nav}>
         <span>Home</span>
         <span>About Us</span>
