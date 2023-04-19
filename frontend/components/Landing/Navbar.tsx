0d46524688550eaab41518641fe969318393c8db
4SAMU <samuelnzomo4@gmail.com>
2023-04-19 16:31:27 +0300

    navbar drawer design update

--- frontend/components/Landing/Navbar.tsx
+++ frontend/components/Landing/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Moving from "../../public/Moving.png";
+import NavLogo from "../../public/FooterLogo.svg";
 import Image from "next/image";
 import styles from "@/styles/Navbar.module.css";
 import mLogo from "../../public/mobileImages/lgo.svg";
@@ -22,6 +23,9 @@ export default function Navbar() {
     x_close,
     open,
     close,
+    a_container,
+    MobileNavlogo,
+    navline,
   } = styles;
 
   React.useEffect(() => {
@@ -78,63 +82,68 @@ export default function Navbar() {
         onClick={toggleDrawer}
       >
         <div className={theMiniNav}>
+          <div className={MobileNavlogo}>
+            <Image src={NavLogo} alt="Movin-Africa" />
+          </div>
+          <div className={navline}></div>
           <button className={x_close} onClick={toggleDrawer}>
             X
           </button>
-          <Link href={"/#Home"}>
-            <div
-              onClick={() => {
-                setIsOpen(!isOpen);
-                setActiveNav("Home");
-              }}
-              className={activeNav === "/" ? styles.active : ""}
-            >
-              Home
-            </div>
-          </Link>
-
-          <Link href="/#AboutUs">
-            <div
-              onClick={() => {
-                setIsOpen(!isOpen);
-                setActiveNav("AboutUs");
-              }}
-              className={activeNav === "AboutUs" ? styles.active : ""}
-            >
-              About Us
-            </div>
-          </Link>
-          <Link href={"/#InvestmentCriteria"}>
-            <div
-              onClick={() => {
-                setIsOpen(!isOpen);
-              }}
-              className={
-                activeNav === "InvestmentCriteria" ? styles.active : ""
-              }
-            >
-              Investment Criteria
-            </div>
-          </Link>
-          <Link href={"/Portfolio"}>
-            <div
-              onClick={() => {
-                setIsOpen(!isOpen);
-              }}
-            >
-              Portfolio
-            </div>
-          </Link>
-          <Link href="/Resources">
-            <div
-              className={activeNav === "Resources" ? styles.active : ""}
-              onClick={() => {
-                setIsOpen(!isOpen);
-              }}
-            >
-              Resources
-            </div>
-          </Link>
+          <div className={a_container}>
+            <Link href={"/#Home"}>
+              <div
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                  setActiveNav("Home");
+                }}
+                className={activeNav === "/" ? styles.active : ""}
+              >
+                Home
+              </div>
+            </Link>
+            <Link href="/#AboutUs">
+              <div
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                  setActiveNav("AboutUs");
+                }}
+                className={activeNav === "AboutUs" ? styles.active : ""}
+              >
+                About Us
+              </div>
+            </Link>
+            <Link href={"/#InvestmentCriteria"}>
+              <div
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+                className={
+                  activeNav === "InvestmentCriteria" ? styles.active : ""
+                }
+              >
+                Investment Criteria
+              </div>
+            </Link>
+            <Link href={"/Portfolio"}>
+              <div
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Portfolio
+              </div>
+            </Link>
+            <Link href="/Resources">
+              <div
+                className={activeNav === "Resources" ? styles.active : ""}
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Resources
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
